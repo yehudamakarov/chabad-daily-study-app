73dd5f9409b13fd4f2b279365e075b7d8974b112
Yehuda Makarov <yehudamakarov@gmail.com>
2018-12-11 23:44:39 -0500

    Tried to set up half typescript setup. need to get a tsx component working, or use other ignite boilerplate

---
+++ App/Containers/LaunchScreen.tsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { Image, ScrollView, Text, View } from "react-native";
+import { Images } from "../Themes";
+
+// Styles
+import styles from "./Styles/LaunchScreenStyles";
+
+export default class LaunchScreen extends Component {
+    render() {
+        return (
+            <View style={styles.mainContainer}>
+                <Image source={Images.background} style={styles.backgroundImage} resizeMode="stretch" />
+                <ScrollView style={styles.container}>
+                    <View style={styles.centered}>
+                        <Image source={Images.launch} style={styles.logo} />
+                    </View>
+
+                    <View style={styles.section} >
+                        <Image source={Images.ready} />
+                        <Text style={styles.sectionText}>
+                            Testing
+            </Text>
+                    </View>
+
+                </ScrollView>
+            </View>
+        );
+    }
+}
